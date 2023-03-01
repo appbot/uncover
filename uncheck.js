5f67cf31e54a093c3b71764113da2132da419a87
Hooky <hooky@qrmaster.co.nz>
2023-03-01 14:15:09 +0800

    Build unchecker
    c8 is an obvious problem

---
+++ uncheck.js
@@ -0,0 +1,18 @@
+const uncheckByBranch = (result, branches) =>
+  branches.forEach(({ lines, hits }) =>
+    hits.forEach((count, i) => count == 0 && (result[lines[i]] = 0))
+  );
+
+const uncheckByFunction = (result, { lines, hits = [] }) =>
+  hits.forEach((count, i) => count == 0 && (result[lines[i]] = 0));
+
+const uncheck = ({ lines, functions, branches }) => {
+  const result = Array.from(lines);
+
+  uncheckByBranch(result, branches || []);
+  uncheckByFunction(result, functions || {});
+
+  return result;
+};
+
+module.exports = { uncheck };
