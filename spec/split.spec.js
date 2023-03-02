d08accadf4daf09234a6542cffec02e5b6892589
Hooky <hooky@qrmaster.co.nz>
2023-03-02 12:32:33 +0800

    Add file segment splitter

---
+++ spec/split.spec.js
@@ -0,0 +1,23 @@
+const split = require('../lib/split');
+
+describe('split', () => {
+  set(
+    'file',
+    `
+body of
+file coverage 1
+
+end_of_record
+body of file coverage 2
+end_of_record
+end_of_record
+    `
+  );
+  subject(() => split(file));
+
+  it('splits on `end_of_record` with trimming', () =>
+    expect(subject).to.eql([
+      'body of\nfile coverage 1',
+      'body of file coverage 2',
+    ]));
+});
