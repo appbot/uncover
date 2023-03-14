90a5adbde099ccfa11bc02168ddf00179e92c066
Hooky <hooky@appbot.co>
2023-03-14 15:21:39 +0800

    Move uncover and serialise in to the class

--- spec/file_coverage.spec.js
+++ spec/file_coverage.spec.js
@@ -141,5 +141,86 @@ describe('FileCoverage', () => {
           ));
       });
     });
+
+    describe('#serialise', () => {
+      beforeEach(() => {
+        coverage.addLine({ line, hits: lineHits });
+        coverage.addLine({ line: line + 1, hits: lineHits });
+        coverage.addBranch({ line, block: 0, branch: 0, hits: 0 });
+        coverage.addFunction({ name: 'myFunc2', line: line + 1, hits: 0 });
+      });
+
+      subject('info', () => coverage.serialise());
+
+      it('produces a correct lcov part', () =>
+        expect(info).to.eq(`TN:
+SF:test.js
+FN:3,myFunc
+FN:4,myFunc2
+FNDA:9,myFunc
+FNDA:0,myFunc2
+FNF:2
+FNH:1
+BRDA:3,0,0,0
+BRDA:3,0,3,11
+BRF:4
+BRH:1
+DA:3,2
+DA:4,1
+LF:2
+LH:2
+end_of_record`));
+
+      context('after uncover', () => {
+        it('matches expected output', () => {
+          coverage.uncover();
+
+          expect(info).to.eq(`TN:
+SF:test.js
+FN:3,myFunc
+FN:4,myFunc2
+FNDA:9,myFunc
+FNDA:0,myFunc2
+FNF:2
+FNH:1
+BRDA:3,0,0,0
+BRDA:3,0,3,11
+BRF:4
+BRH:1
+DA:3,0
+DA:4,0
+LF:2
+LH:0
+end_of_record`);
+        });
+      });
+
+      context('when there is no line coverage', () => {
+        beforeEach(() => coverage.lines.clear());
+
+        it('returns an empty string', () => expect(info).to.eq(''));
+      });
+    });
+  });
+
+  describe('#uncover', () => {
+    subject('uncover', () => coverage.uncover());
+
+    beforeEach(() => {
+      coverage.addLine({ line: 1, hits: 2 });
+      coverage.addLine({ line: 2, hits: 2 });
+      coverage.addLine({ line: 3, hits: 2 });
+      coverage.addBranch({ line: 1, block: 0, branch: 3, hits: 0 });
+      coverage.addBranch({ line: 3, block: 0, branch: 0, hits: 1 });
+      coverage.addFunction({ name: 'myFunc', line: 2, hits: 0 });
+      coverage.addFunction({ name: 'yourFunc', line: 3, hits: 1 });
+    });
+
+    it('zeros lines where branch or fn coverage is zero', () => {
+      uncover;
+      expect(coverage.lines.get(1)).to.eq(0);
+      expect(coverage.lines.get(2)).to.eq(0);
+      expect(coverage.lines.get(3)).to.eq(2);
+    });
   });
 });
